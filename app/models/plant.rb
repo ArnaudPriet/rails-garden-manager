ccd0c8a840fd2fc7e3be3fef1cd6000abe414b84
ArnaudPriet <arnaud.priet1@gmail.com>
2021-09-13 15:03:33 +0200

    test1

--- app/models/plant.rb
+++ app/models/plant.rb
@@ -5,5 +5,4 @@ class Plant < ApplicationRecord
   validates :image_url, presence: true
   URL_REGEXP = /(http|https):\/\/[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?/ix
   validates :image_url, format: { with: URL_REGEXP, message: 'format invalid' }
-  validates
 end
